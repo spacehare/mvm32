0e236ee8851945a2caefdf533530c1237f97ec69
spacehare <dantath425@gmail.com>
2026-05-16 02:16:55 -0400

    Create ab_jump.gd

---
+++ game/ab_jump.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var player: Player
+@export var jumps: int
+@export var jumps_max: int = 2
+@export var jump_height := 5.0
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	printt(jumps, jumps_max)
+	if player.is_on_floor():
+		jumps = jumps_max
+
+	# TODO jump buffer
+
+	if Input.is_action_just_pressed('act_jump') and jumps > 0:
+		jumps -= 1
+		player.velocity.y = jump_height
+	else:
+		player.velocity.y -= gravity * delta
